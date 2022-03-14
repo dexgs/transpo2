fe66dfbc5b25eaf56e54be07fb3bda8ac86990ba
Dexter Gaon-Shatford <dexter@gaonshatford.ca>
2022-03-13 22:33:40 -0400

    reduce timeout to 50ms

--- src/config.rs
+++ src/config.rs
@@ -59,7 +59,7 @@ impl Default for TranspoConfig {
             // 1 Hour
             quota_interval_minutes: 60,
 
-            read_timeout_milliseconds: 500,
+            read_timeout_milliseconds: 50,
 
             storage_dir: PathBuf::from("./transpo_storage"),
 
