78bfbacdfdacbbe4c046c903bd302ab037945665
Dexter Gaon-Shatford <dexter@gaonshatford.ca>
2022-03-15 01:05:44 -0400

    improve enabling service worker

--- www/js/download.js
+++ www/js/download.js
@@ -2,47 +2,50 @@ const downloadForm = document.getElementById("download-form");
 const downloadButton = document.getElementById("download-button");
 
 async function setup(updateWorker) {
+    downloadForm.removeEventListener("submit", downloadEventHandler);
+
     if ("serviceWorker" in navigator) {
         downloadButton.disabled = true;
         downloadButton.classList.add("throbber");
 
-        navigator.serviceWorker.register("./download_worker.js").then(
-            async registration => {
-                downloadForm.removeEventListener("submit", downloadEventHandler);
+        try {
+            let registration = await navigator.serviceWorker.register("./download_worker.js");
+            downloadForm.removeEventListener("submit", downloadEventHandler);
 
-                if (updateWorker) {
-                    registration = await registration.update();
-                }
-
-                downloadButton.classList.remove("throbber");
-                downloadButton.disabled = false;
-
-                const key = await transpoGetKeyFromURL();
-                const uploadID = transpoGetUploadIDFromURL();
-
-                // send the info the service worker needs to decrypt the request
-                // in browser
-                const msg = {
-                    key: key,
-                    uploadID: uploadID
-                };
-
-                if (registration.installing) {
-                    registration.installing.postMessage(msg);
-                } else {
-                    registration.active.postMessage(msg);
-                }
-
-                console.log("registered service worker");
-            },
-            error => {
-                console.error(error);
-                downloadForm.addEventListener("submit", downloadEventHandler);
-
-                downloadButton.classList.remove("throbber");
-                downloadButton.disabled = false;
+            if (updateWorker) {
+                registration = await registration.update();
             }
-        );
+
+            const key = await transpoGetKeyFromURL();
+            const uploadID = transpoGetUploadIDFromURL();
+
+            // send the info the service worker needs to decrypt the request
+            // in browser
+            const msg = {
+                key: key,
+                uploadID: uploadID
+            };
+
+            if (registration.installing) {
+                registration.installing.postMessage(msg);
+            } else {
+                registration.active.postMessage(msg);
+            }
+
+            console.log("registered service worker");
+        } catch (error) {
+            // if the serviceWorker feature is available, but installing failed,
+            // try installing again. In the meantime, enable the non-sw download
+            // handler so downloading still works.
+
+            console.error(error);
+            downloadForm.addEventListener("submit", downloadEventHandler);
+
+            setTimeout(async () => { await setup(true); }, 100);
+        }
+
+        downloadButton.classList.remove("throbber");
+        downloadButton.disabled = false;
     } else {
         downloadForm.addEventListener("submit", downloadEventHandler);
     }
@@ -65,5 +68,4 @@ async function downloadEventHandler(e) {
 }
 
 
-window.addEventListener("pageshow", async () => { await setup(true); });
-downloadForm.addEventListener("submit", async () => { await setup(false); });
+window.addEventListener("pageshow", async () => { await setup(true) });
