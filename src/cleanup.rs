b3fe5de769fe24b8c2bbc5023c9266cbda3bbd26
Dexter Gaon-Shatford <dexter@gaonshatford.ca>
2022-04-02 12:44:19 -0400

    Make cleanup more resiliant
    Make sure that even if the cleanup task thread panics, another one will
    still be spawned.

--- src/cleanup.rs
+++ src/cleanup.rs
@@ -20,36 +20,44 @@ fn cleanup_thread(
     loop {
         thread::sleep(Duration::from_secs(CLEANUP_DELAY_SECS));
 
-        let db_connection = establish_connection(db_backend, &db_url);
+        let storage_path = storage_path.clone();
+        let db_url = db_url.clone();
 
-        if let Some(expired_upload_ids) = Upload::select_expired(&db_connection) {
-            for id in expired_upload_ids {
-                delete_upload_dir(&storage_path, id);
-                Upload::delete_with_id(id, &db_connection);
+        let join_handle = thread::spawn(move || {
+            let db_connection = establish_connection(db_backend, &db_url);
+
+            if let Some(expired_upload_ids) = Upload::select_expired(&db_connection) {
+                for id in expired_upload_ids {
+                    delete_upload_dir(&storage_path, id);
+                    Upload::delete_with_id(id, &db_connection);
+                }
             }
-        }
 
-        // Delete any uploads older than the max upload time. This is to make
-        // sure uploaded files that do not occur in the database get cleaned up.
-        if let Ok(dir_entries) = std::fs::read_dir(&storage_path) {
-            for entry in dir_entries {
-                if let Ok((path, modified_time)) = entry
-                    .and_then(|e| Ok((e.path(), std::fs::metadata(e.path())?)))
-                    .and_then(|(p, m)| Ok((p, m.modified()?)))
-                {
-                    if path.is_dir() {
-                        let now = SystemTime::now();
-                        if let Ok(age) = now.duration_since(modified_time) {
-                            let age_minutes = age.as_secs() as usize / 60;
-                            if age_minutes > max_upload_age_minutes {
-                                if let Err(e) = std::fs::remove_dir_all(path) {
-                                    eprintln!("{}", e);
+            // Delete any uploads older than the max upload time. This is to make
+            // sure uploaded files that do not occur in the database get cleaned up.
+            if let Ok(dir_entries) = std::fs::read_dir(&storage_path) {
+                for entry in dir_entries {
+                    if let Ok((path, modified_time)) = entry
+                        .and_then(|e| Ok((e.path(), std::fs::metadata(e.path())?)))
+                        .and_then(|(p, m)| Ok((p, m.modified()?)))
+                    {
+                        if path.is_dir() {
+                            let now = SystemTime::now();
+                            if let Ok(age) = now.duration_since(modified_time) {
+                                let age_minutes = age.as_secs() as usize / 60;
+                                if age_minutes > max_upload_age_minutes {
+                                    if let Err(e) = std::fs::remove_dir_all(path) {
+                                        eprintln!("{}", e);
+                                    }
                                 }
                             }
                         }
                     }
                 }
             }
+        });
+        if join_handle.join().is_err() {
+            eprintln!("Joining cleanup thread failed");
         }
     }
 }
