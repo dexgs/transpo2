730d6e10d7a6030faf0f9c39d5e385f8c0316686
Dexter Gaon-Shatford <dexter@gaonshatford.ca>
2023-04-11 09:07:13 -0400

    cleanup cleanup.rs

--- src/cleanup.rs
+++ src/cleanup.rs
@@ -8,14 +8,14 @@ use std::path::PathBuf;
 const CLEANUP_DELAY_SECS: u64 = 60 * 60;
 
 pub fn spawn_cleanup_thread(
-    read_timeout_milliseconds: usize, storage_path: PathBuf,
+    read_timeout_ms: usize, storage_path: PathBuf,
     db_backend: DbBackend, db_url: String)
 {
-    thread::spawn(move || cleanup_thread(read_timeout_milliseconds, storage_path, db_backend, db_url));
+    thread::spawn(move || cleanup_thread(read_timeout_ms, storage_path, db_backend, db_url));
 }
 
 fn cleanup_thread(
-    read_timeout_milliseconds: usize, storage_path: PathBuf,
+    read_timeout_ms: usize, storage_path: PathBuf,
     db_backend: DbBackend, db_url: String)
 {
     loop {
@@ -24,48 +24,52 @@ fn cleanup_thread(
         let storage_path = storage_path.clone();
         let db_url = db_url.clone();
 
-        thread::spawn(move || {
-            let db_connection = establish_connection(db_backend, &db_url);
+        thread::spawn(move || cleanup(read_timeout_ms, storage_path, db_backend, db_url));
+    }
+}
 
-            if let Some(expired_upload_ids) = Upload::select_expired(&db_connection) {
-                for id in expired_upload_ids {
-                    Upload::delete_with_id(id, &db_connection);
-                    delete_upload_dir(&storage_path, id);
-                }
-            }
+fn cleanup(
+    read_timeout_ms: usize, storage_path: PathBuf, db_backend: DbBackend, db_url: String)
+{
+    let db_connection = establish_connection(db_backend, &db_url);
 
-            // Detect broken uploads by the following criteria:
-            // - There is a directory for the upload whose name is a valid ID.
-            // - There is no record of an upload with said ID in the database.
-            // - The time since the upload was modified *exceeds* the maximum
-            //   amount of time Transpo permits between writes, i.e. we can be
-            //   reasonably sure that the upload is not currently in progress.
-            if let Ok(dir_entries) = std::fs::read_dir(&storage_path) {
-                for entry in dir_entries {
-                    if let Some((id, path, modified_time)) = entry.ok()
-                        .and_then(|e| Some((e.path(), std::fs::metadata(e.path().join("upload")).ok()?)))
-                        .and_then(|(p, m)| Some((p, m.modified().ok()?)))
-                        .and_then(|(p, m)| Some((i64_from_b64_bytes(p.file_name()?.to_str()?.as_bytes())?, p, m)))
-                    {
-                        if path.is_dir() && Upload::select_with_id(id, &db_connection).is_none() {
-                            let now = SystemTime::now();
-                            if let Ok(age_millis) = now.duration_since(modified_time).map(|d| d.as_millis()) {
+    if let Some(expired_upload_ids) = Upload::select_expired(&db_connection) {
+        for id in expired_upload_ids {
+            Upload::delete_with_id(id, &db_connection);
+            delete_upload_dir(&storage_path, id);
+        }
+    }
 
-                                // Depending on various factors, the modified_time
-                                // which gets reported can be slightly behind,
-                                // so we give at least 5 seconds of wiggle room.
-                                let write_deadline = std::cmp::max(5000, read_timeout_milliseconds);
+    // Detect broken uploads by the following criteria:
+    // - There is a directory for the upload whose name is a valid ID.
+    // - There is no record of an upload with said ID in the database.
+    // - The time since the upload was modified *exceeds* the maximum
+    //   amount of time Transpo permits between writes, i.e. we can be
+    //   reasonably sure that the upload is not currently in progress.
+    if let Ok(dir_entries) = std::fs::read_dir(&storage_path) {
+        for entry in dir_entries {
+            let entry_data = entry.ok()
+                .and_then(|e| Some((e.path(), std::fs::metadata(e.path().join("upload")).ok()?)))
+                .and_then(|(p, m)| Some((p, m.modified().ok()?)))
+                .and_then(|(p, m)| Some((i64_from_b64_bytes(p.file_name()?.to_str()?.as_bytes())?, p, m)));
 
-                                if age_millis as usize > write_deadline
-                                    && Upload::select_with_id(id, &db_connection).is_none()
-                                {
-                                    delete_upload_dir(&storage_path, id);
-                                }
-                            }
+            if let Some((id, path, modified_time)) = entry_data {
+                if path.is_dir() && Upload::select_with_id(id, &db_connection).is_none() {
+                    let now = SystemTime::now();
+                    if let Ok(age_millis) = now.duration_since(modified_time).map(|d| d.as_millis()) {
+                        // Depending on various factors, the modified_time
+                        // which gets reported can be slightly behind,
+                        // so we give at least 5 seconds of wiggle room.
+                        let write_deadline = 5000 + read_timeout_ms;
+
+                        if age_millis as usize > write_deadline
+                            && Upload::select_with_id(id, &db_connection).is_none()
+                        {
+                            delete_upload_dir(&storage_path, id);
                         }
                     }
                 }
             }
-        });
+        }
     }
 }
