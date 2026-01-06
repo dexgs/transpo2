8e150de29aa49dbae7dd444d0a1f8bfb5ff1e92f
Dexter Gaon-Shatford <dexter@gaonshatford.ca>
2026-01-06 14:53:06 -0500

    fix lock order to prevent possible deadlock

--- src/concurrency.rs
+++ src/concurrency.rs
@@ -1,5 +1,5 @@
 use std::sync::{Arc, RwLock, RwLockReadGuard};
-use dashmap::DashMap;
+use dashmap::{DashMap, mapref::entry::Entry};
 
 
 // Count the number of concurrent accessors to files to make sure that they
@@ -19,6 +19,7 @@ impl Accessor {
 }
 
 pub struct AccessorMutex {
+    id: i64,
     mtx: Arc<RwLock<Accessor>>,
     parent: Accessors
 }
@@ -31,13 +32,12 @@ impl AccessorMutex {
 
 impl Drop for AccessorMutex {
     fn drop(&mut self) {
-        let mut accessor = self.mtx.write().unwrap();
-
-        accessor.rc -= 1;
-        if accessor.rc == 0 {
-            let map = &self.parent.0;
-            map.remove(&accessor.id);
-        }
+        let map = &self.parent.0;
+        map.remove_if(&self.id, |_, _| {
+            let mut accessor = self.mtx.write().unwrap();
+            accessor.rc -= 1;
+            accessor.rc == 0
+        });
     }
 }
 
@@ -52,40 +52,29 @@ impl Accessors {
 
     pub fn access(&self, id: i64) -> AccessorMutex {
         let map = &self.0;
-
-        // Get the existing mutex, or create it if it does not exist (or is poisoned)
-        let accessor_mutex = match map.get(&id) {
-            Some(accessor_mutex) => {
-                match accessor_mutex.write() {
-                    Ok(mut accessor) => {
-                        accessor.rc += 1;
-                        accessor_mutex.clone()
-                    }
-                    Err(_) => {
-                        // Handle a poisoned lock...
-                        let accessor = Accessor {
-                            id,
-                            rc: 1
-                        };
-                        let accessor_mutex = Arc::new(RwLock::new(accessor));
-                        accessor_mutex
-                    }
+        let mtx = match map.entry(id) {
+            Entry::Occupied(e) => {
+                let mtx = e.get().clone();
+                {
+                    let mut accessor = mtx.write().unwrap();
+                    accessor.rc += 1;
                 }
+                mtx
             },
-            None => {
-                let accessor = Accessor {
+            Entry::Vacant(e) => {
+                let inner = Accessor {
                     id,
                     rc: 1
                 };
-                let accessor_mutex = Arc::new(RwLock::new(accessor));
-                accessor_mutex
+                let mtx = Arc::new(RwLock::new(inner));
+                e.insert(mtx.clone());
+                mtx
             }
         };
 
-        map.insert(id, accessor_mutex.clone());
-
         AccessorMutex {
-            mtx: accessor_mutex,
+            id: id,
+            mtx: mtx,
             parent: self.clone()
         }
     }
